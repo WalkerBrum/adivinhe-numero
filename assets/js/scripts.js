5210dc9ff50a2c6506c10cbe7a476eb7afaee58d
Walker Brum Lobato Filho <walker.b.lobato@gmail.com>
2022-08-28 14:00:07 -0300

    Sendind number to input with click

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,19 @@
-scripts
+const btnSendNumber = document.querySelector('#send-number');
+const number = document.querySelector('#number');
+
+btnSendNumber.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const number = document.querySelector('#enter-number');
+
+    const value = number.value;
+    changeNumber(value);
+    console.log(value);
+});
+
+const changeNumber = (value) => {
+
+    if (isNaN(value)) return;
+
+    number.innerHTML = value;
+}
