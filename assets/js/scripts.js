a893b939a5e714a9adee75f7d24913ecda82adfa
Walker Lobato <walker.b.lobato@gmail.com>
2022-08-29 11:55:34 -0300

    Function that consumes API that fetches random number to be guessed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,16 @@
 const btnSendNumber = document.querySelector('#send-number');
 const number = document.querySelector('#number');
+let numberAleatory;
 
 btnSendNumber.addEventListener("click", function(e) {
+
     e.preventDefault();
 
     const number = document.querySelector('#enter-number');
 
     const value = number.value;
     changeNumber(value);
-    console.log(value);
+    console.log(numberAleatory)
 });
 
 const changeNumber = (value) => {
@@ -17,3 +19,19 @@ const changeNumber = (value) => {
 
     number.innerHTML = value;
 }
+
+async function getNumber() {
+
+    const response = await fetch('https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300');
+    const json = await response.json();
+
+    return json.value
+}
+
+window.onload = async () => {
+
+    response = await getNumber();
+    
+    numberAleatory = response;
+}
+
